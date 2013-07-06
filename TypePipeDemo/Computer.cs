db6454dabb57d46e95260fd14cc28544235f7312
Julian Lettner <julian.lettner@gmail.com>
2013-07-06 20:00:47 +0200

    Initial commit.

---
+++ TypePipeDemo/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace TypePipeDemo
+{
+  public class Computer
+  {
+    public virtual void SurfTheWeb (string url)
+    {
+      var webClient = new WebClient();
+      var content = webClient.DownloadString (url);
+
+      if (content.Contains ("shit") || content.Contains ("fuck"))
+        Console.WriteLine ("I know what is good for you! Redirect to: www.myLittlePony.com");
+      else
+        Console.WriteLine ("Go on ... but be carefull, big brother is watching.");
+    }
+
+    public virtual int ComputeMeaningOfLife ()
+    {
+      return 42;
+    }
+  }
+}
---
+++ TypePipeDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TypePipeDemo
+{
+  class Program
+  {
+    static void Main ()
+    {
+      var computer = new Computer();
+
+      computer.SurfTheWeb ("http://www.commitLogsFromLastNight.com");
+      Console.WriteLine();
+
+      Console.WriteLine ("The meaning of life is {0}.", computer.ComputeMeaningOfLife());
+      Console.WriteLine ("The meaning of life is STILL {0}.", computer.ComputeMeaningOfLife());
+      Console.WriteLine();
+    }
+  }
+}
